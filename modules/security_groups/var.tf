9d681c9f3d23a9ff0653609849b5d545450d8e4b
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-17 13:15:53 +0530

    Update var.tf

--- modules/security_groups/var.tf
+++ modules/security_groups/var.tf
@@ -1,9 +1,4 @@
-variable "web_sg_name" {
-  description = "Name for the web server security group"
-  type        = string
-}
 variable "vpc_id" {
-  description = "value"
-  type = string
-  
+  description = "The ID of the VPC"
+  type        = string
 }
