c6f071294c8f9e4bfc4337ef880b2838d6125e60
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-17 13:15:35 +0530

    Update outputs.tf

--- modules/security_groups/outputs.tf
+++ modules/security_groups/outputs.tf
@@ -1,9 +1,4 @@
-output "web_sg_id" {
-  description = "The ID of the web security group"
-  value       = aws_security_group.web_sg.id
-}
-
-output "db_sg_id" {
-  description = "The ID of the database security group"
-  value       = aws_security_group.db_sg.id
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
 }
