5e43c1a188af84ed53ab62bdd945c0bfdd3331d6
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-17 13:29:44 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -51,18 +51,3 @@ resource "aws_instance" "web_server_2" {
               echo "Hello from Web Server 2" | sudo tee /usr/share/nginx/html/index.html > /dev/null
               EOF
 }
-
-resource "aws_instance" "web" {
-  count         = length(var.subnet_ids)
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_ids[count.index]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              systemctl start apache2
-              systemctl enable apache2
-              EOF
-}
