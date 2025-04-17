b5cffe84f071f0a0e5e062704d08ca2feea7a12f
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-17 12:21:42 +0530

    Create var.tf

---
+++ modules/security_groups/var.tf
@@ -0,0 +1,14 @@
+variable "web_sg_name" {
+  description = "Name for the web server security group"
+  type        = string
+}
+
+variable "rds_sg_name" {
+  description = "Name for the RDS security group"
+  type        = string
+}
+variable "vpc_id" {
+  description = "value"
+  type = string
+  
+}
