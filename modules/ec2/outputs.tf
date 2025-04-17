b2ae7b971f8abfdd78743ed63c183fa900983531
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-17 13:10:07 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,3 +1,9 @@
-output "instance_ids" {
-  value = aws_instance.web[*].id
+output "web_server_1_id" {
+  description = "The ID of Web Server 1"
+  value       = aws_instance.web_server_1.id
+}
+
+output "web_server_2_id" {
+  description = "The ID of Web Server 2"
+  value       = aws_instance.web_server_2.id
 }
