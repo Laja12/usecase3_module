8cdcc9b5095172b34bb4d1d0c55f8fabeea32f4d
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-17 12:32:36 +0530

    Update var.tf

--- modules/security_groups/var.tf
+++ modules/security_groups/var.tf
@@ -2,11 +2,6 @@ variable "web_sg_name" {
   description = "Name for the web server security group"
   type        = string
 }
-
-variable "rds_sg_name" {
-  description = "Name for the RDS security group"
-  type        = string
-}
 variable "vpc_id" {
   description = "value"
   type = string
