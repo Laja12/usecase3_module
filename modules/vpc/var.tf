40a0bfa4635968f233e3176a0ce1291f90e0b138
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-17 13:17:55 +0530

    Update var.tf

--- modules/vpc/var.tf
+++ modules/vpc/var.tf
@@ -1,14 +1,24 @@
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "The CIDR block for the VPC"
   type        = string
 }
 
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
+variable "public_subnet_cidr_1" {
+  description = "The CIDR block for public subnet 1"
+  type        = string
 }
 
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
+variable "public_subnet_cidr_2" {
+  description = "The CIDR block for public subnet 2"
+  type        = string
+}
+
+variable "private_subnet_cidr_1" {
+  description = "The CIDR block for private subnet 1"
+  type        = string
+}
+
+variable "private_subnet_cidr_2" {
+  description = "The CIDR block for private subnet 2"
+  type        = string
 }
